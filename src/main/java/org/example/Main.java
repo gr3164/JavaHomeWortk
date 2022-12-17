1cee9280ffd0aa7b29ed7ca0a2d444021e7c9eb6
Grisha_Pinigin <pin.pinigin@mail.ru>
2022-12-17 22:59:44 +0500

    Add mergeSort method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,48 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        int[] arr = {123, 1, 44, 22, 3, 4343, 2, 1, 11, 234};
+        int lngthArr = arr.length;
+        mergeSort(arr, lngthArr);
+        System.out.println(Arrays.toString(arr));
+    }
+    /**
+     * Реализовать алгоритм сортировки слиянием
+     */
+    public static void mergeSort(int[] src, int n) {
+        if (n < 2) return;
+
+        int mid = n / 2;
+        int[] left = new int[mid];
+        int[] right = new int[n - mid];
+
+        System.arraycopy(src, 0, left, 0, mid);
+        System.arraycopy(src, mid, right, 0, n - mid);
+
+        mergeSort(left, mid);
+        mergeSort(right, n - mid);
+
+        merge(src, left, right, mid, n - mid);
+    }
+    public static void merge(int[] src, int[] left, int[] right, int leftLength, int rightLength) {
+        int k = 0, i = 0, j = 0;
+
+        while (i < leftLength && j < rightLength) {
+            if (left[i] <= right[j]) src[k++] = left[i++];
+            else src[k++] = right[j++];
+        }
+
+        while (i < leftLength) {
+            src[k++] = left[i++];
+        }
+
+        while (j < rightLength) {
+            src[k++] = right[j++];
+        }
     }
 }
